2deb711712af5c2892b1266fffb9adc2386214c7
amitfin <19599059+amitfin@users.noreply.github.com>
2024-03-06 00:43:55 +0000

    Metadata update

--- custom_components/oref_alert/metadata/area_to_polygon.py
+++ custom_components/oref_alert/metadata/area_to_polygon.py
@@ -1,4 +1,5 @@
 """Helper for loading area to polygon map."""
+
 import json
 from os import path
 import zipfile
@@ -8,11 +9,10 @@ from shapely.geometry import Point, Polygon
 
 def _load_area_to_polygon() -> dict[str, list[list[float]]]:
     """Return the map of area to list of tupples with (lat, long) coordinates."""
-    with zipfile.ZipFile(
-        f"{path.splitext(__file__)[0]}.json.zip"
-    ) as zip_file, zip_file.open(
-        f"{path.splitext(path.basename(__file__))[0]}.json"
-    ) as json_file:
+    with (
+        zipfile.ZipFile(f"{path.splitext(__file__)[0]}.json.zip") as zip_file,
+        zip_file.open(f"{path.splitext(path.basename(__file__))[0]}.json") as json_file,
+    ):
         return json.load(json_file)
 
 
