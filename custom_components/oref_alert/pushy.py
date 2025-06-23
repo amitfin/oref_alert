1f926dcb95359c5c6bd81d0a7a668048d628a9df
amitfin <amittein@gmail.com>
2025-06-23 22:47:12 +0000

    pushy component

---
+++ custom_components/oref_alert/pushy.py
@@ -0,0 +1,162 @@
+"""Pushy notification channel."""
+
+from __future__ import annotations
+
+import hashlib
+from itertools import chain
+from typing import TYPE_CHECKING, Final
+
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.instance_id import async_get
+
+from custom_components.oref_alert.metadata.area_info import AREA_INFO
+
+from .const import CONF_AREAS, CONF_SENSORS, LOGGER
+
+if TYPE_CHECKING:
+    from homeassistant.config_entries import ConfigEntry
+    from homeassistant.core import HomeAssistant
+
+API_ENDPOINT: Final = "https://pushy.ioref.app"
+MQTT_ENDPOINT: Final = "mqtt-{ts}.ioref.io:443"
+PUSHY_CREDENTIALS_KEY: Final = "pushy_credentials"
+ANDROID_ID_KEY: Final = "androidId"
+REGISTRATION_PARAMETERS: Final = {
+    ANDROID_ID_KEY: None,  # To be filled
+    "app": None,
+    "appId": "66c20ac875260a035a3af7b2",
+    "platform": "android",
+    "sdk": 10117,
+}
+ANDROID_ID_SUFFIX: Final = "-Google-Android-SDK-built-for-x86_64"
+TOPICS_KEY: Final = "topics"
+
+
+async def get_device_id(hass: HomeAssistant) -> str:
+    """Return a stable ID of 16 characters."""
+    ha_id = await async_get(hass)
+    return hashlib.blake2b(ha_id.encode("utf-8"), digest_size=8).hexdigest()
+
+
+class PushyNotifications:
+    """Register for notifications coming from Pushy."""
+
+    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+        """Initialize areas checker."""
+        self._hass = hass
+        self._config_entry = config_entry
+        self._http_client = async_get_clientsession(hass)
+        self._credentials: dict = {}
+        self._topics: list = []
+
+    async def _register(self) -> None:
+        """Perform a new registration and save it in the config."""
+        device_id = await get_device_id(self._hass)
+        try:
+            async with self._http_client.post(
+                f"{API_ENDPOINT}/register",
+                json={
+                    **REGISTRATION_PARAMETERS,
+                    ANDROID_ID_KEY: f"{device_id}{ANDROID_ID_SUFFIX}",
+                },
+            ) as response:
+                credentials = await response.json()
+        except:  # noqa: E722
+            LOGGER.exception(f"'{API_ENDPOINT}/register' failed")
+            return
+        # Save the credentials data which causes the integration to reload.
+        if self._hass.config_entries.async_update_entry(
+            self._config_entry,
+            data={
+                **(self._config_entry.data or {}),
+                PUSHY_CREDENTIALS_KEY: credentials,
+            },
+        ):
+            self._hass.config_entries.async_schedule_reload(self._config_entry.entry_id)
+        return
+
+    async def _validate(self, credentials: dict) -> bool:
+        """Validate that the configuration is working properly."""
+        device_id = await get_device_id(self._hass)
+        try:
+            await self._http_client.post(
+                f"{API_ENDPOINT}/devices/auth",
+                json={
+                    **REGISTRATION_PARAMETERS,
+                    ANDROID_ID_KEY: f"{device_id}{ANDROID_ID_SUFFIX}",
+                    **credentials,
+                },
+            )
+        except:  # noqa: E722
+            # Delete the credentials data which causes the integration to reload.
+            # Note: there is no loop here. The registration might fail and that's it.
+            if self._hass.config_entries.async_update_entry(
+                self._config_entry,
+                data={
+                    key: value
+                    for key, value in (self._config_entry.data or {}).items()
+                    if key != PUSHY_CREDENTIALS_KEY
+                },
+            ):
+                self._hass.config_entries.async_schedule_reload(
+                    self._config_entry.entry_id
+                )
+            return False
+        return True
+
+    async def _subscribe(self) -> None:
+        """Subscribe to the relevant topics."""
+        self._topics = [
+            str(AREA_INFO[area]["segment"])
+            for area in (
+                self._config_entry.options[CONF_AREAS]
+                + list(
+                    chain.from_iterable(
+                        self._config_entry.options.get(CONF_SENSORS, {}).values()
+                    )
+                )
+            )
+            if area in AREA_INFO
+        ]
+        try:
+            await self._http_client.post(
+                f"{API_ENDPOINT}/subscribe",
+                json={
+                    **self._credentials,
+                    TOPICS_KEY: self._topics,
+                },
+            )
+        except:  # noqa: E722
+            LOGGER.exception(f"'{API_ENDPOINT}/subscribe' failed")
+            self._topics = []
+
+    async def _unsubscribe(self) -> None:
+        """Unsubscribe the relevant topics."""
+        if not self._topics:
+            return
+        try:
+            await self._http_client.post(
+                f"{API_ENDPOINT}/unsubscribe",
+                json={
+                    **self._credentials,
+                    TOPICS_KEY: self._topics,
+                },
+            )
+        except:  # noqa: E722
+            LOGGER.exception(f"'{API_ENDPOINT}/unsubscribe' failed")
+
+    async def start(self) -> None:
+        """Register for notifications."""
+        if (
+            credentials := (self._config_entry.data or {}).get(PUSHY_CREDENTIALS_KEY)
+        ) is None:
+            await self._register()
+            return
+        if not (await self._validate(credentials)):
+            return
+        self._credentials = credentials
+        await self._subscribe()
+
+    async def stop(self) -> None:
+        """Unregister."""
+        await self._unsubscribe()
---
+++ tests/test_pushy.py
@@ -0,0 +1,172 @@
+"""Tests for push file."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from aiohttp.client_exceptions import (
+    ClientError,
+)
+from pytest_homeassistant_custom_component.common import (
+    MockConfigEntry,
+)
+from pytest_homeassistant_custom_component.test_util.aiohttp import (
+    AiohttpClientMockResponse,
+)
+from yarl import URL
+
+from custom_components.oref_alert.const import (
+    CONF_ALERT_ACTIVE_DURATION,
+    CONF_AREAS,
+    CONF_SENSORS,
+    DOMAIN,
+)
+from custom_components.oref_alert.metadata.area_info import AREA_INFO
+from custom_components.oref_alert.metadata.segment_to_area import SEGMENT_TO_AREA
+from custom_components.oref_alert.pushy import (
+    ANDROID_ID_SUFFIX,
+    API_ENDPOINT,
+    get_device_id,
+)
+
+if TYPE_CHECKING:
+    from homeassistant.config_entries import ConfigEntry
+    from homeassistant.core import HomeAssistant
+    from pytest_homeassistant_custom_component.test_util.aiohttp import (
+        AiohttpClientMocker,
+    )
+
+DEFAULT_OPTIONS = {CONF_AREAS: ["קריית אונו"], CONF_ALERT_ACTIVE_DURATION: 10}
+
+
+def mock_api_calls(aioclient_mock: AiohttpClientMocker) -> None:
+    """Mock relevant URLs of APIs."""
+    if aioclient_mock:
+        for uri in ("register", "devices/auth", "subscribe", "unsubscribe"):
+            aioclient_mock.post(f"{API_ENDPOINT}/{uri}")
+
+
+async def setup_test(
+    hass: HomeAssistant,
+    aioclient_mock: AiohttpClientMocker | None = None,
+    data: dict | None = None,
+    options: dict | None = None,
+) -> ConfigEntry:
+    """Set up steps for a test case."""
+    if aioclient_mock:
+        mock_api_calls(aioclient_mock)
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, data=(data or {}), options={**DEFAULT_OPTIONS, **(options or {})}
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done(wait_background_tasks=True)
+    return config_entry
+
+
+async def cleanup_test(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Clean up steps for a test case."""
+    assert await hass.config_entries.async_remove(config_entry.entry_id)
+    await hass.async_block_till_done(wait_background_tasks=True)
+
+
+async def test_device_id(hass: HomeAssistant) -> None:
+    """Test device ID."""
+    id1 = await get_device_id(hass)
+    assert len(id1) == 16
+    id2 = await get_device_id(hass)
+    assert id1 == id2
+
+
+async def test_registration_params(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+) -> None:
+    """Test registration parameters."""
+    config = await setup_test(hass, aioclient_mock)
+    await cleanup_test(hass, config)
+    for method, url, data, _headers in aioclient_mock.mock_calls:
+        if method == "POST" and url == URL(f"{API_ENDPOINT}/register"):
+            assert data["androidId"].endswith(ANDROID_ID_SUFFIX)
+            assert len(data["androidId"]) == len(ANDROID_ID_SUFFIX) + 16
+            assert data["platform"] == "android"
+            break
+    else:
+        msg = "Registration call was not found"
+        raise AssertionError(msg)
+
+
+async def test_registration_persistency(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+) -> None:
+    """Test registration persistency."""
+    aioclient_mock.post(f"{API_ENDPOINT}/register", text='{"token": "a", "auth": "b"}')
+    config = await setup_test(hass, aioclient_mock)
+    assert config.data["pushy_credentials"] == {"token": "a", "auth": "b"}
+    await cleanup_test(hass, config)
+
+
+async def test_subscribe_unsubscribe(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+) -> None:
+    """Test subscribe/unsubscribe calls."""
+    aioclient_mock.post(f"{API_ENDPOINT}/register", text="{}")
+    config = await setup_test(
+        hass, aioclient_mock, options={CONF_SENSORS: {"Oref Alert Test": ["פתח תקווה"]}}
+    )
+    await cleanup_test(hass, config)
+
+    segments = [
+        str(AREA_INFO["קריית אונו"]["segment"]),
+        str(AREA_INFO["פתח תקווה"]["segment"]),
+    ]
+    assert SEGMENT_TO_AREA[int(segments[0])] == "קריית אונו"
+    assert SEGMENT_TO_AREA[int(segments[1])] == "פתח תקווה"
+
+    for uri in ("subscribe", "unsubscribe"):
+        for method, url, data, _headers in aioclient_mock.mock_calls:
+            if method == "POST" and url == URL(f"{API_ENDPOINT}/{uri}"):
+                assert data["topics"] == segments
+                break
+        else:
+            msg = f"{uri} call was not found"
+            raise AssertionError(msg)
+
+
+async def test_validation_failure(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+) -> None:
+    """Test validation failure."""
+
+    async def validate(method: str, url: URL, data: dict) -> AiohttpClientMockResponse:
+        response = AiohttpClientMockResponse(method, url)
+        if data["auth"] != "good":
+            response.exc = ClientError
+        return response
+
+    aioclient_mock.post(f"{API_ENDPOINT}/devices/auth", side_effect=validate)
+    aioclient_mock.post(f"{API_ENDPOINT}/register", text='{"auth": "good"}')
+    config = await setup_test(
+        hass, aioclient_mock, data={"pushy_credentials": {"auth": "bad"}}
+    )
+    await cleanup_test(hass, config)
+    assert config.data["pushy_credentials"] == {"auth": "good"}
+
+
+async def test_subscribe_failure(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+) -> None:
+    """Test subscribe failure."""
+    aioclient_mock.post(f"{API_ENDPOINT}/subscribe", exc=ClientError)
+    config = await setup_test(hass, aioclient_mock, data={"pushy_credentials": {}})
+    await cleanup_test(hass, config)
+    for method, url, _data, _headers in aioclient_mock.mock_calls:
+        assert method != "POST" or url != URL(f"{API_ENDPOINT}/unsubscribe")
+
+
+async def test_unsubscribe_failure(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+) -> None:
+    """Test unsubscribe failure."""
+    aioclient_mock.post(f"{API_ENDPOINT}/unsubscribe", exc=ClientError)
+    config = await setup_test(hass, aioclient_mock, data={"pushy_credentials": {}})
+    await cleanup_test(hass, config)
